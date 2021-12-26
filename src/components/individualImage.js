ac36ae4000dde3a1db4410e3e683a4abbbb9d84b
matidsc <dmatiutu211102@gmail.com>
2021-12-26 00:41:09 -0300

    Footer y navBar adaptadas a desktop y mobile, faltan detalles, animaciones y servicios

--- src/components/individualImage.js
+++ src/components/individualImage.js
@@ -6,41 +6,49 @@ import { IoIosArrowDroprightCircle } from "react-icons/io";
 
 import { useState } from 'react';
 const IndividualImage = (props) => {
-
-
+  
     const [actual, setActual] = useState(0)
     const imageList = []
     for (var i = 0; i < 25; i++) {
         imageList.push("" + i + "")
     }
+
     var length = imageList.length
     const nextImg = () => {
         setActual(actual === length - 1 ? 0 : actual + 1)
     }
     const prevImg = () => {
-        setActual(actual === 0 ? length - 1 : actual - 1)
+        setActual(actual === 0 ? length -= 1 : actual - 1)
     }
     console.log(actual)
+    const defaultImage =(event)=>{
 
+    console.log('TRIIGEREDDD')
+        event.target.src= '../img/logoPrincipal.jpeg'
+        
+    }
     return (
-    <div className='imgWrapper'>
-        <IoIosArrowDropleftCircle size='45px' className='arrow arrowL' size="40" onClick={prevImg} />
-        <IoIosArrowDroprightCircle size='45px' className='arrow arrowR' size="40" onClick={nextImg} />
+        <div className='imgWrapper'>
+            <IoIosArrowDropleftCircle  className='arrow arrowL' size="40" onClick={prevImg} />
+            <IoIosArrowDroprightCircle className='arrow arrowR' size="40" onClick={nextImg} />
 
-        {
-            imageList.map((img, index) => {
-                
-                return (
-                    <div className={index === actual ? 'imgActiva' : 'slide'}>
-                        {index===actual && <img key={index} className='imgMain' src={require("../img/Portafolio/" + img + ".jpg").default} />
-}
-                    </div>
-                )
-            })
-        }
+            {
+                imageList.map((img, index) => {
 
-    </div>
+                    return (
+                        <div key={index} className={index === actual ? 'imgActiva' : 'slide'}>
+                            
+                            {index === actual && <img key={index} onError={defaultImage} className='imgMain' src={require("../img/Portafolio/" + img + ".jpg").default} />
+                            }
+                        </div>
+                        
+                    )
+                })
+            }
+
+        </div>
     )
+
     /*<img src={require('../images/' + fileName + '.jpg')} />
            <img src={require(`../images/${fileName}.jpg`)} />*/
 
