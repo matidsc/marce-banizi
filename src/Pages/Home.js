2e01d614e25fecc466fc1fc3fe9fb1fcae08bb42
matidsc <dmatiutu211102@gmail.com>
2022-02-27 16:58:54 -0300

    .

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,10 +6,10 @@ import imgRandom from '../img/imgBack.jpg'
 const Home = () => {
     var tops = [], lefts = []
     const imgsRandom = []
-    for (let i = 0; i < 50; i++) {imgsRandom.push(i)}
+    for (let i = 0; i < 20; i++) {imgsRandom.push(i)}
 
-    /*Valores iniciales*/
-    var left = Math.floor((Math.random() * ((window.innerWidth - 50) - 50)))
+    /*VALORES INICIALES*/
+    var left = Math.floor((Math.random() * ((document.documentElement.scrollHeight - 50) - 50)))
     var top = Math.floor(Math.random() 
             * ((document.documentElement.scrollHeight
             - (document.documentElement.scrollHeight * 15) / 100) - 50))
@@ -17,50 +17,55 @@ const Home = () => {
     
     tops.push(top)
     lefts.push(left)
-    var leftDef, topDef
 
     return (
 
         <main className='HomeWrapper' id='pageWrapper'>
 
             {imgsRandom.map((index) => {
+                console.log('prueba'+tops.length)
+                //CALCULAR COORDENADAS ALEATORIAS
 
                 top     = Math.floor(Math.random() 
                         * ((document.documentElement.scrollHeight
                         - (document.documentElement.scrollHeight * 15) / 100) - 50))
                         + (document.documentElement.scrollHeight * 15) / 100;
-                
                 left = Math.floor((Math.random() * ((document.documentElement.scrollWidth - 50) - 50)));
-                console.log('IMAGEN NUMERO: ' + index)
-                console.log('DIFERENCIA LEFT ' + (Math.abs(lefts[index] - left)));
-                
-                console.log('DIFERENCIA TOP ' + (Math.abs(tops[index] - top)));
-                //CALCULAR COORDENADAS DE NUEVO COMPONENTE
-                
-                for (let i = 0; i < tops.length-1; i++) {
-                    console.log('LARGO'+tops.length)
-                    if (((Math.abs(tops[i] - top) >= 50) && (Math.abs(lefts[i] - left)) >= 50)) {
 
-                        leftDef = Math.abs(lefts[index])
-                        topDef = Math.abs(tops[index])
-                        console.log(leftDef+'---'+ topDef)
+                console.log('IMAGEN NUMERO: ' + tops.length)
+                console.log('DIFERENCIA LEFT ' + (Math.abs(lefts[index] - left)));
+                console.log('DIFERENCIA TOP ' + (Math.abs(tops[index] - top)));
+                
+                //VERIFICACIÓN DE COORDENADAS
+                for (let i = 0; i < tops.length-1; i++) {
+
+                    console.log('iteracion del loop:' +i)
+
+                    if (((Math.abs(tops[i] - top) >= 50) && (Math.abs(lefts[i] - left)) >= 50)) {
+                        console.log('LA DIFERENCIA ES MAYOR')
+                        tops.push(Math.abs(tops[index]))
+                        lefts.push(Math.abs(lefts[index]))
+                    }
+                    else{
+                       tops.push(1)
+                       lefts.push(1)
+                       console.log('LA DIFERENCIA ES MENOR')
+
                     }
                 }
-
-                tops.push(topDef)
-                lefts.push(leftDef)
 
                 return <img alt={index} key={index} style={
 
                     {
                         position: 'absolute',
-                        top: '' + topDef + 'px',
-                        left: '' + leftDef + 'px',
+                        top: '' + tops[index] + 'px',
+                        left: '' + tops[index] + 'px',
                         zIndex: '1'
 
                     }} width='50px' src={imgRandom} />
 
             })}
+            
             <img src={imgHome} className='imgHome'></img>
         </main>
     )
