080660d1c33584ba0d2301d51bb41ac814e951cd
agustin-perez <agperez.prieto@protonmail.com>
2022-05-25 12:50:54 -0300

    Database Changes

--- database/ddl.sql
+++ database/ddl.sql
@@ -25,18 +25,18 @@
 	) ENGINE = InnoDB DEFAULT CHARSET = UTF8; 
 
 	CREATE TABLE img_servicio_cat_bck (
-		id CHAR(16) NOT NULL,
-		img_id CHAR(16) NOT NULL,
-		data MEDIUMBLOB,	
+		id MEDIUMINT NOT NULL,
+		img_id MEDIUMINT NOT NULL,
+		url MEDIUMTEXT,	
 		PRIMARY KEY (id),
 
 		CONSTRAINT fk_img_servicio_cat_bck_id FOREIGN KEY (id) REFERENCES servicio_cat_bck (id)
 	) ENGINE = InnoDB DEFAULT CHARSET = UTF8; 
 
 	CREATE TABLE img_subservicio_cat_bck (
-		id CHAR(16) NOT NULL,
-		img_id CHAR(16) NOT NULL,
-		data MEDIUMBLOB,	
+		id MEDIUMINT NOT NULL,
+		img_id MEDIUMINT NOT NULL,
+		url MEDIUMTEXT,	
 		PRIMARY KEY (id),
 
 		CONSTRAINT fk_img_subservicio_cat_bck_id FOREIGN KEY (id) REFERENCES subservicio_cat_bck (id)
