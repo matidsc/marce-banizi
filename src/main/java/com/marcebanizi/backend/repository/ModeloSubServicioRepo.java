c463b4b8635f8677bf6d80d03392b49200a0a092
agustin-perez <agperez.prieto@protonmail.com>
2022-06-06 01:24:54 -0300

    v1.0.2 bug fixes

--- src/main/java/com/marcebanizi/backend/repository/ModeloSubServicioRepo.java
+++ src/main/java/com/marcebanizi/backend/repository/ModeloSubServicioRepo.java
@@ -12,7 +12,7 @@ public interface ModeloSubServicioRepo extends JpaRepository<ModeloSubServicio, 
            nativeQuery = true)
    List<ModeloSubServicio> getSubServicios();
    @Query(
-           value = "select ids, subservicio_cat_bck.id, subservicio_cat_bck.nombre, descripcion, precio, url from subservicio_cat_bck  left join img_subservicio_cat_bck on img_subservicio_cat_bck.idi=subservicio_cat_bck.imagenMuestra where subservicio_cat_bck.id = ?1",
+           value = "select ids, subservicio_cat_bck.id, subservicio_cat_bck.nombre, descripcion, precio, url from subservicio_cat_bck  left join img_subservicio_cat_bck on img_subservicio_cat_bck.idi=subservicio_cat_bck.imagenMuestra where subservicio_cat_bck.ids = ?1",
            nativeQuery = true)
    ModeloSubServicio getSubServiciosId(Long id);
    @Query(
