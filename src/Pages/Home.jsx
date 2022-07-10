a271e0b5b1f831f8d3a610d067b5d7630fb74af3
matidsc <dmatiutu211102@gmail.com>
2022-07-10 04:11:17 -0300

    mejora visual en home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,8 @@ import imgHome from "../img/ef908db4-4e22-4030-bd87-445dfadd13e1.jpeg";
 import PageWrapper from "../containers/pageWrapper";
 import HomeCard from "../components/homeCard";
 import '../style/HomeCard.css'
+import {IoIosArrowForward} from 'react-icons/io'
+import {Link} from 'react-router-dom'
 const Home = () => {
   const [img, setimg] = useState()
   useEffect(() => {
@@ -33,9 +35,13 @@ const Home = () => {
         <HomeCard  imagen={img} />
         <HomeCard imagen={img} />
         <HomeCard imagen={img} />
-        <HomeCard imagen={img} />
-        <div>
-
+        <div className="portafolioBtn">
+          <Link to='/'>
+          <div className="portafolioBtnContent">
+          <span>Ver portafolio</span>
+          <IoIosArrowForward/>
+          </div>
+          </Link>
         </div>
       </div>
       </div>
--- src/components/homeCard.jsx
+++ src/components/homeCard.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-const HomeCard = ({imagen}) => {
-    return (
-        <div className='homeCardWrapper'>
+import { Link } from 'react-router-dom'
+const HomeCard = ({ imagen }) => {
+  return (
+    <div className='homeCardWrapper'>
 
-            <span>Servicio</span>
-            <img src={imagen} alt="" />
-            <button className="btnMasInformacion">
+<div className='cardContent'>
+      <span>Servicio</span>
+
+      <img src={imagen} alt="" />
+      </div>
+      <button className="btnMasInformacion">
         <Link
           style={{ display: "block", color: "white" }}
           to={`/`}
         >
-          Conocer más
+          Conocé más
         </Link>
       </button>
-        </div>
-    )
+    </div>
+  )
 }
 
 export default HomeCard
